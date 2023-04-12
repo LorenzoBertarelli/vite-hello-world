a3d44c58ffc685d7b9163691e9f32116735c59d5
LorenzoBertarelli <lorenzobertarelli59@gmail.com>
2023-04-12 15:53:52 +0200

    Esercizio finito

---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: "AppJumbo",
+    data() {
+        return {
+            text: "Ciao sono il Jumbotron"
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+  <p class="jumbotron">{{ text }}</p>
+</template>
+
+<style>
+.jumbotron {
+    font-size: 5rem;
+    text-align: center;
+    background-color: yellow;
+    padding: 100px;
+}
+</style>
